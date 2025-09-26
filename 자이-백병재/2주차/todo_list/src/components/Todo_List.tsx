961c8f22a71498c84654dc961cfec2735fc3e186
Baek <bjaeng@naver.com>
2025-09-26 21:40:29 +0900

    Feat : useList hook 추가

--- 자이-백병재/2주차/todo_list/src/components/Todo_List.tsx
+++ 자이-백병재/2주차/todo_list/src/components/Todo_List.tsx
@@ -1,36 +1,10 @@
-import React, { useState, type JSX } from "react";
-import type { Items } from "../types/Items";
-import List_Component from "./List_component";
+import { type JSX } from "react";
+import List_Component from "./List_Component";
 import Input_Form from "./Input_Form";
+import { useList } from "../hooks/useList";
 
 const Todo_List = (): JSX.Element => {
-    const [todo_list, setTodo_list] = useState<Items[]>([]);    // useState를 이용, Items[]형식의 todo_list
-    const [done_list, setDone_list] = useState<Items[]>([]);    // useState를 이용, Items[]형식의 todo_list
-
-    const addTodoList = (text: string) => {                  // 입력받기
-        const newTodo: Items = {                                // 입력값을 newTodo로 가져옴
-            id: Date.now(),
-            text,
-            complete: false,
-        };
-        setTodo_list([...todo_list, newTodo]);                  // todo_list에 newTodo값 추가
-    }
-
-    const doneTodo = (id: number) => {                                  // 완료하기
-        const doneItem = todo_list.find(item => item.id === id);        // 완료한 아이템 찾기
-        if (!doneItem) return;                                          // 항목이 없으면 함수 종료
-
-        const newTodo_list = todo_list.filter(item => item.id !== id);  // 필터링
-        setTodo_list(newTodo_list);                                     // todo_list 수정
-
-        setDone_list([...done_list, {...doneItem, complete: true}]);    // done_list 수정
-
-    }
-
-    const deleteDone = (id: number) => {
-        const newDone_list = done_list.filter(item => item.id !== id);  // 필터링
-        setDone_list(newDone_list);
-    }
+    const {todo_list, done_list, addTodoList, doneTodo, deleteDone} = useList();    // hook을 이용해 한번에 state, 함수 받기
 
     return (
     <div id="root_container">
