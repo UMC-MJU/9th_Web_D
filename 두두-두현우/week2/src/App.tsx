6491634c25f99530fdd177ed110fa800519f74b9
HyunwooDoo <engusdn0421@gmail.com>
2025-09-28 14:38:07 +0900

    🎨 Style: App.tsx 파일 생성

--- 두두-두현우/week2/src/App.tsx
+++ 두두-두현우/week2/src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div className="container">
+      {/* Header */}
+      <h1 className="title">TODO</h1>
+      <h2 className="subtitle">할 일을 효율적으로 관리하세요</h2>
+    </div>
   );
 }
 
