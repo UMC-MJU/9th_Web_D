797363d43230d2f46138c8941f0a286c76869894
HyunwooDoo <engusdn0421@gmail.com>
2025-10-01 20:46:57 +0900

    🎨 Design: 페이지 경로 조정

--- 두두-두현우/week3/spaRoute/src/App.tsx
+++ 두두-두현우/week3/spaRoute/src/App.tsx
@@ -1,15 +1,23 @@
 import "./App.css";
+import { Link, Route, Routes } from "./hooks";
 
+const LandingPage = () => <h1>DUDU SPA </h1>;
 const DUDUPage = () => <h1>두두 페이지</h1>;
-const HomePage = () => <h1>홈 페이지</h1>;
 const MemoPage = () => <h1>메모장</h1>;
 const DiaryPage = () => <h1>일기장</h1>;
 
 const Header = () => {
   return (
-    <nav style={{ display: "flex", gap: "10px" }}>
+    <nav
+      style={{
+        display: "flex",
+        gap: "10px",
+        padding: "20px",
+        borderBottom: "1px solid #ccc",
+      }}
+    >
+      <Link to="/">HOME</Link>
       <Link to="/dudu">DUDU</Link>
-      <Link to="/home">HOME</Link>
       <Link to="/memo">MEMO</Link>
       <Link to="/diary">DIARY</Link>
     </nav>
@@ -21,8 +29,8 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="/" component={LandingPage} />
         <Route path="/dudu" component={DUDUPage} />
-        <Route path="/home" component={HomePage} />
         <Route path="/memo" component={MemoPage} />
         <Route path="/diary" component={DiaryPage} />
       </Routes>
