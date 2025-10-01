e68511df2a18f77d2a0d014056afb616f8192cfc
Baek <bjaeng@naver.com>
2025-10-01 22:03:01 +0900

    Feat : App.tsx 작성

--- 자이-백병재/3주차/workBook/src/App.tsx
+++ 자이-백병재/3주차/workBook/src/App.tsx
@@ -1,10 +1,29 @@
-import './App.css'
+import './index.css'
+import MainPage from './components/MainPage'
+import NewPage_1 from './components/NewPage_1'
+import NewPage_2 from './components/NewPage_2'
+import { useRouter } from './hooks/useRouter'
+
+const urls = {
+  "/": <MainPage />,
+  "/1": <NewPage_1 />,
+  "/2": <NewPage_2 />
+}
 
 function App() {
+  const { path, push } = useRouter();
 
   return (
-    <>
-    </>
+    <div>
+      <nav>
+        <button onClick={() => push('/')}>MainPage</button>
+        <button onClick={() => push('/1')}>NewPage_1</button>
+        <button onClick={() => push('/2')}>NewPage_2</button>        
+      </nav>
+      <main>
+        { urls[path] || <h1>Error!</h1>}
+      </main>
+    </ div>
   )
 }
 
