7afedcd651876c15c8355aa44988133c59f01992
Baek <bjaeng@naver.com>
2025-10-01 21:18:34 +0900

    Feat : 페이지 컴포넌트 추가

---
+++ 자이-백병재/3주차/workBook/src/components/MainPage.tsx
@@ -0,0 +1,11 @@
+const MainPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen bg-white">
+      <h1 className="text-4xl font-bold text-gray-800 sm:text-5xl md:text-6xl">
+        메인 페이지
+      </h1>
+    </div>
+  );
+};
+
+export default MainPage;
