db02664d698c4e8bacfbcdc3fc129caf18313aee
HyunwooDoo <engusdn0421@gmail.com>
2025-10-01 20:50:39 +0900

    ✨ Feat: SPA 라우팅 컴포넌트 정의

--- 두두-두현우/week3/spaRoute/src/router/Router.tsx
+++ 두두-두현우/week3/spaRoute/src/router/Router.tsx
@@ -1,8 +1,32 @@
-import { Children, cloneElement, useMemo, type ReactElement } from "react";
+import {
+  Children,
+  cloneElement,
+  useMemo,
+  type ReactElement,
+  type FC,
+} from "react";
+import type { RoutesProps, RouteProps } from "../types/types";
+import { useCurrentPath } from "../hooks/hooks";
+
+// 라우트 엘리먼트인지 확인하는 헬퍼 함수
+const isRouteElement = (
+  element: unknown
+): element is ReactElement<RouteProps> => {
+  return !!(
+    element &&
+    typeof element === "object" &&
+    element !== null &&
+    "props" in element &&
+    element.props &&
+    typeof element.props === "object" &&
+    element.props !== null &&
+    "path" in element.props
+  );
+};
 
 // Router.tsx
-export const Routes: FC<RoutesProp> = ({ children }) => {
-  const currentPath = useCurrentPat();
+export const Routes: FC<RoutesProps> = ({ children }) => {
+  const currentPath = useCurrentPath();
   const activeRoute = useMemo(() => {
     const routes = Children.toArray(children).filter(isRouteElement);
     return routes.find((route) => route.props.path === currentPath);
