b4c2c756f9fd35458b2af4b394452d873335a92e
HyunwooDoo <engusdn0421@gmail.com>
2025-10-01 20:22:47 +0900

    ✨ Feat: 라우팅 구조 설정

--- 두두-두현우/week3/spaRoute/src/App.tsx
+++ 두두-두현우/week3/spaRoute/src/App.tsx
@@ -1,9 +1,31 @@
 import "./App.css";
 
+const DUDUPage = () => <h1>두두 페이지</h1>;
+const HomePage = () => <h1>홈 페이지</h1>;
+const MemoPage = () => <h1>메모장</h1>;
+const DiaryPage = () => <h1>일기장</h1>;
+
+const Header = () => {
+  return (
+    <nav style={{ display: "flex", gap: "10px" }}>
+      <Link to="/dudu">DUDU</Link>
+      <Link to="/home">HOME</Link>
+      <Link to="/memo">MEMO</Link>
+      <Link to="/diary">DIARY</Link>
+    </nav>
+  );
+};
+
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
+      <Header />
+      <Routes>
+        <Route path="/dudu" component={DUDUPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/memo" component={MemoPage} />
+        <Route path="/diary" component={DiaryPage} />
+      </Routes>
     </>
   );
 }
