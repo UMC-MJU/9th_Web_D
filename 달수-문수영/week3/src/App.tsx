991a689872b428d23b6b52372083bddf2cf75b2f
달수 <emily4542@naver.com>
2025-10-02 00:40:53 +0900

    TailwindCSS설치!

--- 달수-문수영/week3/src/App.tsx
+++ 달수-문수영/week3/src/App.tsx
@@ -1,34 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full bg-purple-500 text-white p-4 font-bold text-center">
+      {/*가로 전체 폭 지정, 배경색 보라색 팔레트 500단계, 텍스트 흰색, p-4 모든 방향 패딩, 글씨 두껍게, 텍스트정렬 */}
+      <h1>춘식이의 고구마 코딩</h1>
+    </div>
   )
 }
 
