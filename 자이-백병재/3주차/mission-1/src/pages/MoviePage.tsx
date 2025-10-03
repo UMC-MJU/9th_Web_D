7b7eef4bc5d16b5394f0d11ca6b0ec86176ce4cd
Baek <bjaeng@naver.com>
2025-10-03 13:57:22 +0900

    ✨ Feat : API 화면 구성

--- 자이-백병재/3주차/mission-1/src/pages/MoviePage.tsx
+++ 자이-백병재/3주차/mission-1/src/pages/MoviePage.tsx
@@ -16,8 +16,26 @@ const MoviePage = () => {
       }; 
       getMovieAPI();
     }, []);
-    
-  return( <h1>영화 데이터 불러오기</h1>);
+
+  return(
+    <div>
+      <div>
+        {movie.map((movieItem) => (
+          <div key={movieItem.title} className="w-[200px]">
+            {/* 영화 포스터 */}
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movieItem.poster_path}`}
+              alt={`${movieItem.title} poster`}
+              style={{ width: '100%' }}/>
+            {/* 영화 제목 */}
+            <h3>{movieItem.title}</h3>
+            {/* 영화 설명 */}
+            <p>{movieItem.overview}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+    );
 };
 
 export default MoviePage;
