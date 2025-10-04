219de25d293a8839546ee1c76773416640527b8a
HyunwooDoo <engusdn0421@gmail.com>
2025-10-05 00:50:52 +0900

    🎨 Design: 404 페이지 디자인 수정

--- 두두-두현우/week3/mission/src/pages/NotFound.tsx
+++ 두두-두현우/week3/mission/src/pages/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
         </p>
         <Link
           to="/"
-          className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition-colors duration-200"
+          className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition-colors duration-200 cursor-pointer"
         >
           Go Home
         </Link>
