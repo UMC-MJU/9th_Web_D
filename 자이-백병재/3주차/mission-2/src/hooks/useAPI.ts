af70715eb42faec84db96e0b0c44ca27f1c135f7
Baek <bjaeng@naver.com>
2025-10-04 17:53:35 +0900

    ✨ Feat : 페이지 변경 컴포넌트 생성

--- 자이-백병재/3주차/mission-2/src/hooks/useAPI.ts
+++ 자이-백병재/3주차/mission-2/src/hooks/useAPI.ts
@@ -18,6 +18,7 @@ export default function useAPI( pageNum : number = 1 ) {
                 },
             });
             setMovie(data.results);
+            setIsError(false);           
         } catch {
             setIsError(true);
         } finally {
--- 자이-백병재/3주차/mission-2/src/pages/MoviePage.tsx
+++ 자이-백병재/3주차/mission-2/src/pages/MoviePage.tsx
@@ -1,22 +1,33 @@
 import { useState } from "react";
 import useAPI from "../hooks/useAPI";
-import MovieList from "../components/movieList";
+import MovieList from "../components/MovieList";
 import LoadingSpinner from "../components/LoadingSpinner";
 import ErrorAppear from "../components/ErrorAppear";
+import PageButton from "../components/PageButton";
 
 const MoviePage = () => {
   const [pageNum, setPageNum] = useState(1);
   const { movie, isLoading, isError }  = useAPI(pageNum);
 
-  if(isLoading) {
-    return (<LoadingSpinner />);
-  }
-  else if(isError) {
-    return (<ErrorAppear />);
-  }
-  else {
-  return(<MovieList movie={movie}/>);
-  }  
-};
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingSpinner />;
+    }
+    else if (isError) {
+      return <ErrorAppear />;
+    }
+    return <MovieList movie={movie} />;
+  };
+
+  return (
+    <div>
+      <PageButton pageNum={pageNum} setPageNum={setPageNum} />
+      <div className="content-area">
+        {renderContent()}
+      </div>
+    </div>
+  );
+}
 
 export default MoviePage;
