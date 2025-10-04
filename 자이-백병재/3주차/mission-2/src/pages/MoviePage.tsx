1581a00d14e9d3d4e3158c2ce396b74130aa1f4c
Baek <bjaeng@naver.com>
2025-10-04 15:40:55 +0900

    ✨ Feat : MovieList 컴포넌트 생성

--- 자이-백병재/3주차/mission-2/src/pages/MoviePage.tsx
+++ 자이-백병재/3주차/mission-2/src/pages/MoviePage.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import useAPI from "../hooks/useAPI";
+import MovieList from "../components/movieList";
 
 const MoviePage = () => {
   const [pageNum, setPageNum] = useState(1);
-
   const { movie, isLoading, isError }  = useAPI(pageNum);
 
   if(isLoading) {
@@ -13,32 +13,8 @@ const MoviePage = () => {
     return (<p>에러 발생!!</p>);
   }
   else {
-  return(
-    <div>
-      <div className="flex flex-wrap m-5 gap-5 justify-center">
-        {movie.map((movieItem) => (
-          <div key={movieItem.title} 
-          className=" p-2.5 w-[100px] md:w-[200px] xl:w-[300px] group relative rounded-3xl overflow-hidden">
-            {/* 영화 포스터 이미지 */}
-            <img
-              src={`https://image.tmdb.org/t/p/w200${movieItem.poster_path}`}
-              alt={`${movieItem.title} poster`}
-              style={{ width: '100%' }}
-              className=" group-hover:blur-md group-hover:brightness-25 rounded-3xl w-full h-full transition-all duration-200 "
-            />
-            <div className="absolute px-10 py-20 inset-0 flex flex-col justify-between 
-            opacity-0 group-hover:opacity-100 transition-all duration-200 text-amber-50">
-              {/* 영화 제목 */}
-              <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold line-clamp-2">{movieItem.title}</h3>
-              {/* 영화 설명 */}
-              <p className="text-sm md:text-md lg:text-lg line-clamp-2">{movieItem.overview}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-    );
-  }
+  return(<MovieList movie={movie}/>);
+  }  
 };
 
 export default MoviePage;
