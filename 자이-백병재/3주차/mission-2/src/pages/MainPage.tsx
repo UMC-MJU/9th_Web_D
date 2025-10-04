14cd58eb8b32c8c807c513bf1536e3839b750df1
Baek <bjaeng@naver.com>
2025-10-04 20:20:19 +0900

    ✨ Feat : NavBar 컴포넌트 생성

--- 자이-백병재/3주차/mission-2/src/pages/MainPage.tsx
+++ 자이-백병재/3주차/mission-2/src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import CategoryNav from "../components/CategoryNav";
 
 const MainPage = () => {
 
     return (
     <div>
+        <CategoryNav />
         <Outlet />
     </div>);
 }
