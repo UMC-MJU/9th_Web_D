d6021020db3d202b1c5745e3aeacc7918b59d6a4
달수 <emily4542@naver.com>
2025-10-05 15:03:58 +0900

    ✨ Feat: 페이지네이션 버튼 및 현재 페이지 표시 기능 추가

--- 달수-문수영/week3/mission-0/src/pages/MoviePage.tsx
+++ 달수-문수영/week3/mission-0/src/pages/MoviePage.tsx
@@ -10,6 +10,8 @@ export default function MoviePage() {
     const [isPending, setIsPending] = useState(false);
     ///2. 에러 상태
     const [isError, setIsError] = useState(false);
+    ///3.페이지
+    const [page, setPage] = useState(1);
     
     useEffect(():void => {
         const fetchMovies = async (): Promise<void> => {
@@ -17,7 +19,7 @@ export default function MoviePage() {
 
         try{       
             const {data} = await axios.get<MovieResponse>(
-                    'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+                    `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`,
                     {
                         headers: {
                             'Authorization': `Bearer ${import.meta.env.VITE_TMDB_KEY}`
@@ -35,13 +37,40 @@ export default function MoviePage() {
         };
 
         fetchMovies();
-    },[]);
+    },[page]);
+
+    if(isError) {
+        return(
+            <div>
+                <span className='text-red-500'>Error loading movies</span>
+            </div>
+        )
+    }
 
     return(
-        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+        <>
+         <div className='flex items-center justify-center gap-6 mt-5'>
+            <button
+                className='bg-[#169514] text-black px-6 py-3 rounded-lg shadow-md
+                hover:bg-[#61915e] transition-all duration-200 disabled:bg-[#ccc]
+                cursor-pointer disabled:cursor-not-allowed'
+                disabled={page === 1}
+                onClick={():void => setPage((prev):number => prev - 1)}
+                >{'<'}</button>
+            <span>{page}페이지</span>
+            <button
+                className='bg-[#b2dab1] text-black px-6 py-3 rounded-lg shadow-md
+                hover:bg-[#9cc699] transition-all duration-200 cursor-pointer'
+                onClick={():void => setPage((prev):number => prev + 1)}
+                >{'>'}</button>
+         </div>
+        
+        <div 
+            className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
             {movies && movies.map((movie) => (
                 <MoiveCard key={movie.id} movie={movie} />
             ))}
         </div>
+        </>
     );
 }
