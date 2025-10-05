2fc007d19d67f940ba67e0f364ad25b6019569bc
Baek <bjaeng@naver.com>
2025-10-05 21:09:44 +0900

    🔧 Chore : 세부사항을 위한 API 개선

--- 자이-백병재/3주차/mission-3/src/types/movie.ts
+++ 자이-백병재/3주차/mission-3/src/types/movie.ts
@@ -3,4 +3,7 @@ export type Movie = {
   overview : string;    // 영화 설명
   poster_path : string; // 영화 포스터
   id : number;          // 영화 id
+  backdrop_path : string// 영화 배경그림
+  vote_average : number // 영화 평점
+  vote_count : number   // 평점 참여 인원
 };
