2a83a3f5fcde505b5b7b6b238c0adfcd7098e64b
달수 <emily4542@naver.com>
2025-10-05 22:28:50 +0900

    ✨ Feat: MovieDetails, CreditsResponse 인터페이스 생성

--- 달수-문수영/week3/mission-0/src/types/movie.ts
+++ 달수-문수영/week3/mission-0/src/types/movie.ts
@@ -22,3 +22,36 @@ export type Movie = {
     total_pages: number;
     total_results: number;
   };
+
+// 상세 정보 타입
+export interface MovieDetails {
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  runtime: number | null;
+  genres: { id: number; name: string }[];
+  vote_average: number;
+  vote_count: number;
+}
+
+// 크레딧 타입
+export interface CreditsResponse {
+  id: number;
+  cast: Array<{
+    id: number;
+    name: string;
+    character: string;
+    profile_path: string | null;
+  }>;
+  crew: Array<{
+    id: number;
+    name: string;
+    job: string;
+    department: string;
+    profile_path: string | null;
+  }>;
+}
