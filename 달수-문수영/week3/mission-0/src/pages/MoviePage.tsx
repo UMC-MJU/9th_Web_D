9cf7677406572d02c072a7baf5c2449d751e75ef
달수 <emily4542@naver.com>
2025-10-05 14:36:26 +0900

    ✨ Feat: 로딩 및 에러 처리 기능 추가

--- 달수-문수영/week3/mission-0/src/pages/MoviePage.tsx
+++ 달수-문수영/week3/mission-0/src/pages/MoviePage.tsx
@@ -5,18 +5,33 @@ import MoiveCard from '../components/MoiveCard';
 
 export default function MoviePage() {
     const [movies, setMovies] = useState<Movie[]>([]);
+
+    ///1. 로딩 상태
+    const [isPending, setIsPending] = useState(false);
+    ///2. 에러 상태
+    const [isError, setIsError] = useState(false);
     
     useEffect(():void => {
         const fetchMovies = async (): Promise<void> => {
+            setIsPending(true);
+
+        try{       
             const {data} = await axios.get<MovieResponse>(
-                'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
-                {
-                    headers: {
-                        'Authorization': `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+                    'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+                        },
                     }
-                }
-            );
-            setMovies(data.results);
+                );
+
+                setMovies(data.results);
+            }  catch{
+                setIsError(true);
+            } finally {
+                setIsPending(false);
+            }
+
         };
 
         fetchMovies();
