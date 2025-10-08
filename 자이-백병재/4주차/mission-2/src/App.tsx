48d79c867745eba7323e2c5a13cbcc749f90a76f
Baek <bjaeng@naver.com>
2025-10-09 00:31:22 +0900

    ✨ Feat : SignupPage 구현

--- 자이-백병재/4주차/mission-2/src/App.tsx
+++ 자이-백병재/4주차/mission-2/src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import MainPage from './pages/MainPage'
 import MainLayout from './layouts/MainLayout';
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <MainPage /> },
       { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
     ],
   },
 ]);
