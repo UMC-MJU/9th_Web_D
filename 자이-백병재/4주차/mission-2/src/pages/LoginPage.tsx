9f78f9e9b4fc62819d8d96ef456904ab51298d49
Baek <bjaeng@naver.com>
2025-10-09 02:34:20 +0900

    ✨ Feat : 로그인 오류 구현

--- 자이-백병재/4주차/mission-2/src/pages/LoginPage.tsx
+++ 자이-백병재/4주차/mission-2/src/pages/LoginPage.tsx
@@ -1,29 +1,39 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import useForm from "../forms/useForm";
+import { validateSignup, type UserSignupInformation } from "../utils/validate";
 
 const LoginPage = () => {
+    const { values, errors, touched, getInputProps } = useForm<UserSignupInformation>({
+        initValue: { email: "", password: "" },
+        validate: validateSignup,
+    });
 
     return (
         <div className="flex flex-col items-center justify-center h-full gap-3">
             <div className="flex flex-col items-center justify-center bg-gray-800 rounded-2xl p-10 gap-5">
                 <h2 className="text-white font-bold text-3xl mb-2">Login</h2>
 
-                <input className="flex border border-3 border-gray-200 focus:border-[#FFA900] focus:outline-none 
-                rounded-lg w-xs h-10 p-2.5 text-white items-center justify-center" 
-                type={"email"}
+                <input className="border border-3 border-gray-200 focus:border-[#FFA900] focus:outline-none 
+                rounded-lg w-xs h-10 p-2.5 text-white" 
+                type={"email"} {...getInputProps("email")}
                 placeholder={"e-mail"}
                 />
 
-                <input className="flex border border-3 border-gray-200 focus:border-[#FFA900] focus:outline-none 
-                rounded-lg w-xs h-10 p-2.5 text-white items-center justify-center" 
-                type={"password"}
+                <input className="border border-3 border-gray-200 focus:border-[#FFA900] focus:outline-none 
+                rounded-lg w-xs h-10 p-2.5 text-white" 
+                type={"password"} {...getInputProps("password")}
                 placeholder={"password"}
                 />
 
-                <button className="flex bg-[#FFA900] hover:bg-white focus:outline-none 
-                rounded-lg w-xs h-10 p-2.5 text-black items-center justify-center font-bold text-lg" 
-                type={"submit"}>Login</button>
+                <button className={`flex bg-[#FFA900] hover:bg-white focus:outline-none disabled:bg-gray-500 
+                rounded-lg w-xs h-10 p-2.5 text-black items-center justify-center font-bold text-lg`}
+                type={"submit"} disabled={!(touched?.email && touched?.password)}
+                onClick={() => { if (errors?.email.length === 0 && errors?.password.length === 0) 
+                    {alert(`Welecome ${values.email}!`)}}}
+                >Login</button>
 
-                <h2 className="text-[#FFA900] font-bold text-sm">Incorrect Email or Password</h2>
+                {(errors?.email || errors?.password) && (
+                <h2 className="text-[#FFA900] font-bold text-sm">Incorrect Email or Password</h2> )}
 
                 <div className="flex items-center w-full px-5 my-1">
                     <div className="flex-grow h-px bg-white"></div> 
