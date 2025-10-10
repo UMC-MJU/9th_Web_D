14954d8a4885b09dee6f1382f36116287d142b7a
HyunwooDoo <engusdn0421@gmail.com>
2025-10-10 12:53:08 +0900

    🎨 Style: 레이아웃 적용

--- 두두-두현우/week4/src/App.tsx
+++ 두두-두현우/week4/src/App.tsx
@@ -1,9 +1,13 @@
-const App = () => {
+import Layout from "./Layout";
+
+function App() {
   return (
-    <div>
-      <h1>DUDU</h1>
-    </div>
+    <Layout>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold text-white">Welcome</h1>
+      </div>
+    </Layout>
   );
-};
+}
 
 export default App;
