94de95c64c677ab779a07a8c0e71e16fe4cef739
달수 <emily4542@naver.com>
2025-10-12 13:22:28 +0900

    🎨 style(frontend): HomeLayout 네비게이션 문구를 ‘회원가입’으로 변경하고 푸터 제거

--- 달수-문수영/week4/frontend/src/assets/layouts/HomeLayout.tsx
+++ 달수-문수영/week4/frontend/src/assets/layouts/HomeLayout.tsx
@@ -3,11 +3,10 @@ import { Outlet, redirectDocument } from 'react-router-dom';
 const HomeLayout = () => {
     return(
         <div className='h-dvh flex flex-col'>
-            <nav>네비게이션 바입니다.</nav>
+            <nav>회원가입</nav>
             <main className='flex-1'>
                 <Outlet />
             </main>
-            <footer>푸터</footer>
         </div>
     );
 };
