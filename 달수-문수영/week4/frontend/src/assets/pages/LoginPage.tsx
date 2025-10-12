6919720f2fb039b4cbca26095eac29abb2fcdef9
달수 <emily4542@naver.com>
2025-10-12 13:11:13 +0900

    ✨ feat(frontend): 로그인 페이지 폼 로직 및 입력 검증 기능 추가

--- 달수-문수영/week4/frontend/src/assets/pages/LoginPage.tsx
+++ 달수-문수영/week4/frontend/src/assets/pages/LoginPage.tsx
@@ -1,26 +1,48 @@
+import useForm from '../../hooks/useForm';
+import { validateSignin, type UserSigninInformation } from '../../utils/validate';
+
 const LoginPage = () => {
+    const { values, errors, touched, getInputProps } =
+        useForm<UserSigninInformation>({
+            initialValues: { email: "", password: "" },
+            validate: validateSignin,
+        });
+
     const handleSubmit = () => {
-        // TODO: implement submit logic
+        console.log(values);
     };
+    
+    //오류가 하나라도 있거나 입력갑이 비어있으면 버튼을 비활성화
+    const isDisabled: boolean =
+        Object.values(errors || {}).some((error: string) => error.length > 0) || //오류가 있으면 true
+        Object.values(values).some((value: string) => value === ""); //입력값이 비어있으면 true
 
     return (
         <div className="flex flex-col items-center justify-center h-full gap-4">
             <div className="flex flex-col gap-3">
                 <input
-                    className={'border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm'}
+                    {...getInputProps("email")}
+                    name="email"
+                    className={`border w-[300px] p-[10px] rounded-sm focus:border-[#807bff] ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"}`}
                     type={"email"}
                     placeholder={"이메일"}
                 />
-                <div className="text-red-500 text-sm">이메일 에러</div>
+
+                {errors?.email && touched?.email && (<div className="text-red-500 text-sm">{errors.email}</div>)}
+
                 <input
-                    className={'border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm'}
+                    {...getInputProps("password")}
+                    className={`border w-[300px] p-[10px] rounded-sm focus:border-[#807bff] ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"}`}
                     type={"password"}
                     placeholder={"비밀번호"}
                 />
+
+                {errors?.password && touched?.password && (<div className="text-red-500 text-sm">{errors.password}</div>)}
+
                 <button
                     type="button"
                     onClick={handleSubmit}
-                    disabled={false}
+                    disabled={isDisabled}
                     className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-blue-300">
                         로그인
                 </button>
