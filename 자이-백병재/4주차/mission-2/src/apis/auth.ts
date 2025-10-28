665f43cafd276cf9ae5a8f3cf39db50ac2bd6639
Baek <bjaeng@naver.com>
2025-10-28 22:36:11 +0900

    ✨ Feat : 로그인 기능 api 연결

--- 자이-백병재/4주차/mission-2/src/apis/auth.ts
+++ 자이-백병재/4주차/mission-2/src/apis/auth.ts
@@ -1,7 +1,7 @@
 import type { RequestSignupDto } from './../types/auth';
 import axios from 'axios';
 
-const postSignup = async (
+export const postSignup = async (
     body: RequestSignupDto,
 ): Promise<RequestSignupDto> => {
     const { data } = await axios.post("http://localhost:8000/v1/auth/signup", body);
@@ -9,4 +9,15 @@ const postSignup = async (
     return data;
 };
 
-export default postSignup;
+export const postSignin = async (
+    body: RequestSignupDto,
+): Promise<RequestSignupDto> => {
+    const { data } = await axios.post("http://localhost:8000/v1/auth/signin", body);
+
+    return data;
+};
+
+export const getMyInfo = async () : Promise<RequestSignupDto> => {
+    const { data } = await axios.get("http://localhost:8000/v1/users/me");
+    return data;
+};
