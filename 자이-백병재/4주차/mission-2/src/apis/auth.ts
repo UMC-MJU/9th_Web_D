cdc4e410da97e989147256c8d8a4edfedd73f6be
Baek <bjaeng@naver.com>
2025-11-02 23:17:24 +0900

    🔧 Chore : baseURL 활용하게 수정

--- 자이-백병재/4주차/mission-2/src/apis/auth.ts
+++ 자이-백병재/4주차/mission-2/src/apis/auth.ts
@@ -1,46 +1,31 @@
 import type { RequestSigninDto, RequestSignupDto, ResponseMyInfoDto, ResponseSigninDto, ResponseSignupDto } from './../types/auth';
-import axios from 'axios';
+import { axiosInstance } from './axios';
 
 export const postSignup = async (
     body: RequestSignupDto,
 ): Promise<ResponseSignupDto> => {
-    const { data } = await axios.post(import.meta.env.VITE_SERVER_API_URL + "/v1/auth/signup", body);
-
+    // baseURL이 적용되도록 수정
+    const { data } = await axiosInstance.post("/v1/auth/signup", body);
     return data;
 };
 
 export const postSignin = async (
     body: RequestSigninDto,
 ): Promise<ResponseSigninDto> => {
-    const { data } = await axios.post(import.meta.env.VITE_SERVER_API_URL + "h/v1/auth/signin", body);
-
+    // baseURL이 적용되도록 수정
+    const { data } = await axiosInstance.post("/v1/auth/signin", body);
     return data;
 };
 
 export const postSignout = async () => {
-    const { data } = await axios.post('/v1/auth/sign-out');
-
+    // baseURL이 적용되도록 수정
+    const { data } = await axiosInstance.post('/v1/auth/sign-out');
     return data
 };
 
 export const getMyInfo = async () : Promise<ResponseMyInfoDto> => {
-    const tokenString = localStorage.getItem("accessToken");
-    let token: string | null = null;
-
-    if (!tokenString) {
-        throw new Error("Access token is not found in localStorage.");
-    } else {
-        token = JSON.parse(tokenString);
-    }
-
-    // 요청 헤더(header)에 토큰을 설정하는 config 객체를 만듬.
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    };
-
-    const { data } = await axios.get(import.meta.env.VITE_SERVER_API_URL + "/v1/users/me", config);
+    // baseURL이 적용되도록 수정
+    const { data } = await axiosInstance.get("/v1/users/me");
 
     return data;
 };
