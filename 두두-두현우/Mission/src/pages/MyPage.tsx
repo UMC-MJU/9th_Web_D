8cdeed4cf0c0556be0a1c6e9b43d66a275b0c817
HyunwooDoo <engusdn0421@gmail.com>
2025-11-02 18:44:28 +0900

    ✨ Feat: 마이페이지를 비로그인시 접근하지 못하게 ProtectedRoute처리, NotfoundPage로 리다이렉팅

--- 두두-두현우/Mission/src/pages/MyPage.tsx
+++ 두두-두현우/Mission/src/pages/MyPage.tsx
@@ -1,4 +1,14 @@
-const MyPage = () => {
+import { Navigate } from "react-router-dom";
+
+interface MyPageProps {
+  isLoggedIn: boolean;
+}
+
+const MyPage = ({ isLoggedIn }: MyPageProps) => {
+  if (!isLoggedIn) {
+    return <Navigate to="/404" replace />;
+  }
+
   return (
     <div>
       <h1 className="mt-4 text-white">MyPage</h1>
