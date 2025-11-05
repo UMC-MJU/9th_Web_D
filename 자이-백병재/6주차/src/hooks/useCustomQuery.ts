5e6e006951d51e35ca1da12da531bc0fd47afd43
Baek <bjaeng@naver.com>
2025-11-06 00:25:31 +0900

    ✨ Feat : staleTime 구현

--- 자이-백병재/6주차/src/hooks/useCustomQuery.ts
+++ 자이-백병재/6주차/src/hooks/useCustomQuery.ts
@@ -2,13 +2,25 @@ import { useEffect, useState } from 'react';
 
 // 파라매터 타입 정의
 interface UseCustomQueryParams<T> {
-  queryKey: (string | number)[];    // 캐시 키로 사용될 배열
-  queryFn: () => Promise<T>;        // 데이터를 가져오는 비동기 함수
+    queryKey: (string | number)[];      // 캐시 키로 사용될 배열
+    queryFn: () => Promise<T>;          // 데이터를 가져오는 비동기 함수
+    staleTime?: number;                 // fresh로 간주하기까지 걸리는 시간
+    gcTime?: number;                    // 얼마나 지나야 캐시를 정리하는지
+    retry?: number;                     // 재시도 횟수
+}
+
+// 로컬스토리지 저장할 데이터 구조
+interface CacheEntry<T> {
+    data: T;
+    lastFetchedTime: number;    // staleTime과 비교용
 }
 
 export const useCustomQuery = <T,>({ 
     queryKey, 
-    queryFn 
+    queryFn,
+    staleTime = 30_000,
+    gcTime = 5 * 60 * 1000,
+    retry = 3
 }: UseCustomQueryParams<T>) => {
     const [data, setData] = useState<T | null>(null);             // 데이터
     const [isPending, setIsPending] = useState<boolean>(false);   // 로딩 여부
@@ -18,26 +30,53 @@ export const useCustomQuery = <T,>({
     useEffect(() => {
     // 데이터를 가져오는 함수 정의
     const fetchData = async () => {
-      try {
-        // 초기 설정
-        setIsPending(true);
-        setIsError(false);
-        setError(null);
         
-        // queryFn 실행
-        const result = await queryFn();
-        
-        // 성공 시 데이터 저장
-        setData(result);
+        const currentTime = new Date().getTime();                           // 현재 시간 가져오기
+        const cachedItem = localStorage.getItem(JSON.stringify(queryKey));  // 가지고 있는 아이템 확인
 
-      } catch (err) {
-        // 실패 시 에러 저장
-        setIsError(true);
-        setError(err as Error);
-      } finally {
-        // 로딩 종료
-        setIsPending(false);
-      }
+        if(cachedItem) {
+            try{
+                const cachedData: CacheEntry<T> = JSON.parse(cachedItem);   // 데이터 파싱
+
+                setData(cachedData.data); // 만료시에도 마지막 데이터를 사용
+
+                if(currentTime - cachedData.lastFetchedTime < staleTime) {  // 신선할 경우 종료
+                    setIsPending(false);
+                    return;
+                }
+            } catch(err) {
+                localStorage.removeItem(JSON.stringify(queryKey));
+                setError(err as Error);
+            }   
+        }
+
+        try {
+            // 초기 설정
+            setIsPending(true);
+            setIsError(false);
+            setError(null);
+        
+            // queryFn 실행
+            const result = await queryFn();
+        
+            // 성공 시 데이터 저장
+            setData(result);
+
+            // staleTime을 위한 설정
+            const newCacheData: CacheEntry<T> = {
+                data: result,
+                lastFetchedTime: new Date().getTime(),
+            }
+            localStorage.setItem(JSON.stringify(queryKey), JSON.stringify(newCacheData))
+
+        } catch (err) {
+            // 실패 시 에러 저장
+            setIsError(true);
+            setError(err as Error);
+        } finally {
+            // 로딩 종료
+            setIsPending(false);
+        }
     };
 
     // 함수 실행
