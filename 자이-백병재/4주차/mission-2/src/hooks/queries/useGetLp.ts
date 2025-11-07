e6a97c2d703b44c896cf3fba57343173738cac60
Baek <bjaeng@naver.com>
2025-11-07 20:33:56 +0900

    ✨ Feat : useQuery 구현

---
+++ 자이-백병재/4주차/mission-2/src/hooks/queries/useGetLp.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import type { PaginationDTO } from "../../types/lp";
+import { getLpList } from "../../apis/lp";
+import { QUERY_KEY } from "../../constants/key";
+
+function useGetLp({ cursor, search, order, limit }: PaginationDTO) {
+    return useQuery({
+        queryKey:[QUERY_KEY, search, order],
+        queryFn: () => getLpList({
+            cursor, search, order, limit
+        }),
+        staleTime: 5 * 60 * 1000,
+        gcTime: 10 * 60 * 1000,
+        retry: 3,
+        enabled: Boolean(search),
+    });
+}
+
+export default useGetLp;
