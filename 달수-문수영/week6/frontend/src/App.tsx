4ead8efda7d64130f055ecc3cd75e1a67789e79e
달수 <emily4542@naver.com>
2025-11-11 09:34:47 +0900

    🔧 Chore: Provider 내부에 ReactQueryDevtools 추가 (DEV 환경에서만 표시)

--- 달수-문수영/week6/frontend/src/App.tsx
+++ 달수-문수영/week6/frontend/src/App.tsx
@@ -41,6 +41,7 @@ function App() {
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
 			<ReactQueryDevtools initialIsOpen={false} />
+			{import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
 		</QueryClientProvider>
 	);
 }
