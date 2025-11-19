fd609a6553d8465b3dfee37f3b2105df7c5a67f7
Baek <bjaeng@naver.com>
2025-11-19 22:05:18 +0900

    🔧 Chore : useThrottle callback 사용하게 개선

--- 자이-백병재/4주차/mission-2/src/hooks/useThrottle.ts
+++ 자이-백병재/4주차/mission-2/src/hooks/useThrottle.ts
@@ -1,27 +1,49 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from 'react';
 
-function useThrottle<T>(value: T, delay: number): T {
-  const [throttledValue, setThrottledValue] = useState<T>(value);
-  const lastExecuted = useRef<number>(Date.now());
+function useThrottle(
+  callback: () => void, 
+  delay: number, 
+  deps: any[] = []
+) {
+  const lastRan = useRef(Date.now());
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  // 가장 최신 콜백
+  const savedCallback = useRef(callback);
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
   useEffect(() => {
     const now = Date.now();
-    const timeElapsed = now - lastExecuted.current; //  잔여 대기시간 계산
+    const timeElapsed = now - lastRan.current;
 
+    // 1. 쿨타임이 지남 -> 최신 콜백 실행
     if (timeElapsed >= delay) {
-      setThrottledValue(value);
-      lastExecuted.current = now;
-    } else {
-      const timerId = setTimeout(() => {
-        setThrottledValue(value);
-        lastExecuted.current = Date.now();
-      }, delay - timeElapsed);
+      savedCallback.current();
+      lastRan.current = now;
+    } 
+    // 2. 쿨타임 중 -> 남은 시간만큼 대기
+    else {
+      // 이미 예약된 타이머가 없으면 예약
+      if (!timer.current) {
+        const remainingTime = delay - timeElapsed;
 
-      return () => clearTimeout(timerId);
+        timer.current = setTimeout(() => {
+          savedCallback.current();
+          lastRan.current = Date.now();
+          timer.current = null;
+        }, remainingTime);
+      }
     }
-  }, [value, delay]);
 
-  return throttledValue;
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+        timer.current = null;
+      }
+    };
+  }, [delay, ...deps]);
 }
 
 export default useThrottle;
