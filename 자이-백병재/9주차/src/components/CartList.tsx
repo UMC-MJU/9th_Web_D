fa22b04ed69f3b161728dc4948fd4e060a8061e5
Baek <bjaeng@naver.com>
2025-11-29 22:35:16 +0900

    🔧 Chore : 훅과 연결

--- 자이-백병재/9주차/src/components/CartList.tsx
+++ 자이-백병재/9주차/src/components/CartList.tsx
@@ -1,10 +1,20 @@
-import cartItems from "../constants/cartItems";
+import { useEffect } from "react";
+
 import CartItem from "./CartItem";
+import { useAppDispatch, useSelector } from "../hooks/useCustomRedux";
+import { calculateTotals } from "../slices/cartSlice";
 
 const CartList = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      <ul className="w-full"> 
+    <div className="flex flex-col items-center justify-center mb-10">
+      <ul className="w-full">
         {cartItems.map((item) => (
           <CartItem key={item.id} lp={item} />
         ))}
--- 자이-백병재/9주차/src/components/PriceBox.tsx
+++ 자이-백병재/9주차/src/components/PriceBox.tsx
@@ -1,6 +1,13 @@
+import { useSelector } from "../hooks/useCustomRedux";
 
 const PriceBox = () => {
-    return <div className="p-8 flex justify-end">총 가격: 10000원</div>;
+  const { total } = useSelector((state) => state.cart);
+
+  return (
+    <div className="p-8 flex justify-end text-xl font-bold text-gray-800">
+      총 가격: {total.toLocaleString()}원
+    </div>
+  );
 };
 
 export default PriceBox;
