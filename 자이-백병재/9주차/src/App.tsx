3b3e847e654cbe7d69c1d8aec29c81fe9ba71b61
Baek <bjaeng@naver.com>
2025-11-29 21:37:33 +0900

    ✨ Feat : 화면 구현

--- 자이-백병재/9주차/src/App.tsx
+++ 자이-백병재/9주차/src/App.tsx
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar';
+import CartList from './components/CartList';
 
 function App() {
   return (
     <main className="bg-gray-50 min-h-screen">
       <Navbar />
-
+      <CartList />
     </main>
   );
 }
